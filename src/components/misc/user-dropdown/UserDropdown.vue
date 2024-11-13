a38682710e1fa84d03cf068e1068144f2e788fc8
Windel <windeloira07@yahoo.com>
2024-11-13 11:39:18 +0800

    chore(UserDropdown): remove v-loading directive

--- src/components/misc/user-dropdown/UserDropdown.vue
+++ src/components/misc/user-dropdown/UserDropdown.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
-import { computedAsync } from '@vueuse/core';
 
 import type { User } from '@types';
 import { UserService } from '@services';
@@ -9,19 +8,13 @@ import type { UserFieldProps } from './UserDropdown.types';
 
 const props = defineProps<UserFieldProps>();
 
-const isFetching = ref<boolean>(false);
-
 const options = ref<Record<string, string>[]>([]);
 
 const handleFetch = async () => {
-  isFetching.value = true;
-
   const { data } = await UserService.list(
     () => `role=${props.role}&per_page=1000`,
   );
   const { data: results } = data.value || {};
-
-  isFetching.value = false;
 
   options.value = results.data.map((user: User) => ({
     text: user.name,
@@ -33,12 +26,7 @@ onMounted(handleFetch);
 </script>
 
 <template>
-  <BaseSelect
-    v-loading="isFetching"
-    :options="options"
-    name="user_id"
-    :label="props.label || 'User'"
-  >
+  <BaseSelect :options="options" name="user_id" :label="props.label || 'User'">
     <template v-slot:first>
       <option :value="undefined" disabled>
         Select {{ props.label || 'User' }}
