a472a84d6e7b5db18907088567d6fad8c97cb653
Windel <windeloira07@yahoo.com>
2024-11-13 10:54:09 +0800

    chore(farmers): add return link

--- src/pages/dashboard/farmers/new.vue
+++ src/pages/dashboard/farmers/new.vue
@@ -9,6 +9,13 @@ const router = useRouter();
 </script>
 
 <template>
+  <div class="mt-3 mb-1">
+    <RouterLink to="/dashboard/farmers/" class="small lh-1 d-inline-flex">
+      <i class="bi bi-arrow-left me-2"></i>
+      <span>Return</span>
+    </RouterLink>
+  </div>
+
   <DashboardPageHeader title="New Farmer" sub-title="Farmers" />
 
   <div class="card border-0 shadow">
