1e2342379b58300e12dcf7d3987106aa16225f26
Windel <windeloira07@yahoo.com>
2024-11-13 10:44:08 +0800

    chore: dashboard farmers management

--- src/pages/dashboard/farmers/components/table/FarmersTable.vue
+++ src/pages/dashboard/farmers/components/table/FarmersTable.vue
@@ -1,26 +1,64 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref, watch } from 'vue';
+import { useDebounceFn } from '@vueuse/core';
 
 import type { User } from '@types';
 import { UserService } from '@services';
 
 import FarmersTableDeleteButton from './FarmersTableDeleteButton.vue';
 
-const {
-  isFetching,
-  data,
-  execute: runFetch,
-} = UserService.list({ role: 'farmer' });
+const isFetching = ref<boolean>(false);
 
-const items = computed<User[]>(() => {
-  const { data: results } = data.value || {};
-  const { data: items } = results || {};
+const search = ref<string>('');
 
-  return items || [];
+const users = ref<User[]>([]);
+
+const page = ref<number>(1);
+const perPage = ref<number>(5);
+const total = ref<number>(0);
+
+const parameters = computed<string>(() => {
+  return Object.entries({
+    search: search.value,
+    page: page.value,
+    per_page: perPage.value,
+    role: 'farmer',
+  })
+    .map(([key, value]) => `${key}=${value || ''}`)
+    .join('&');
 });
+
+const handleFetch = async () => {
+  isFetching.value = true;
+
+  const { data } = await UserService.list(parameters);
+  const { data: results } = data.value || {};
+
+  isFetching.value = false;
+
+  users.value = results.data || [];
+
+  page.value = results.current_page || 1;
+  perPage.value = results.per_page || 5;
+  total.value = results.total || 0;
+};
+
+watch(
+  parameters,
+  useDebounceFn(() => handleFetch(), 500),
+  { immediate: true },
+);
 </script>
 
 <template>
+  <div class="mb-4 d-flex align-items justify-content-between">
+    <div class="d-flex align-items-center column-gap-2">
+      <BaseInput name="search" placeholder="Search" />
+    </div>
+
+    <slot name="top-bar:right" />
+  </div>
+
   <div v-loading="isFetching" class="table-responsive">
     <table class="table">
       <thead>
@@ -32,21 +70,24 @@ const items = computed<User[]>(() => {
         </tr>
       </thead>
       <tbody class="table-group-divider">
-        <template v-if="!!items.length">
-          <tr v-for="item in items" :key="item.id">
-            <td>{{ item.name }}</td>
-            <td>{{ item.email || 'N/A' }}</td>
-            <td>{{ item.phone }}</td>
+        <template v-if="!!users.length">
+          <tr v-for="user in users" :key="user.id">
+            <td>{{ user.name }}</td>
+            <td>{{ user.email || 'N/A' }}</td>
+            <td>{{ user.phone }}</td>
             <td class="position-sticky end-0">
               <div class="d-flex align-items-center justify-content-center">
                 <RouterLink
-                  :to="`/dashboard/farmers/${item.id}`"
+                  :to="`/dashboard/farmers/${user.id}`"
                   class="btn btn-link px-2"
                 >
                   <i class="bi bi-pencil"></i>
                 </RouterLink>
 
-                <FarmersTableDeleteButton :id="item.id" @success="runFetch" />
+                <FarmersTableDeleteButton
+                  :id="user.id"
+                  @success="handleFetch"
+                />
               </div>
             </td>
           </tr>
@@ -57,4 +98,6 @@ const items = computed<User[]>(() => {
       </tbody>
     </table>
   </div>
+
+  <BasePagination v-model="page" :per-page="perPage" :total="total" />
 </template>
--- src/pages/dashboard/farmers/index.vue
+++ src/pages/dashboard/farmers/index.vue
@@ -9,28 +9,13 @@ import FarmersTable from './components/table/FarmersTable.vue';
 
   <div class="card border-0 shadow">
     <div class="card-body">
-      <div class="d-flex align-items justify-content-between">
-        <BaseInput name="search" placeholder="Search" />
-        <RouterLink to="/dashboard/farmers/new/" class="btn btn-primary"
-          >Add New</RouterLink
-        >
-      </div>
-
-      <FarmersTable class="mt-4" />
-
-      <nav aria-label="Page navigation example">
-        <ul class="pagination my-2 justify-content-center">
-          <li class="page-item disabled">
-            <a class="page-link">Previous</a>
-          </li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item">
-            <a class="page-link" href="#">Next</a>
-          </li>
-        </ul>
-      </nav>
+      <FarmersTable class="mt-4">
+        <template v-slot:top-bar:right>
+          <RouterLink to="/dashboard/farmers/new/" class="btn btn-primary"
+            >Add New</RouterLink
+          >
+        </template>
+      </FarmersTable>
     </div>
   </div>
 </template>
