40ce1fd932cd429fc8071c65066c64716a5a21d9
Windex <windeloira07@gmail.com>
2024-12-02 16:51:09 +0800

    style(DashboardNavbar): remove navbar shadow

--- src/components/dashboard/navbar/DashboardNavbar.vue
+++ src/components/dashboard/navbar/DashboardNavbar.vue
@@ -4,24 +4,8 @@ import DashboardNavbarNav from './DashboardNavbarNav.vue';
 
 <template>
   <above
-    class="dashboard-navbar navbar fixed-top justify-content-end py-2 px-3 rounded-2"
+    class="dashboard-navbar navbar bg-light fixed-top justify-content-end py-2 px-3 rounded-2"
   >
     <DashboardNavbarNav />
   </above>
 </template>
-
-<style scoped>
-.dashboard-navbar {
-  will-change: transform;
-  transition: -webkit-transform 0.2s ease-in-out;
-  transition: transform 0.2s ease-in-out;
-  transition:
-    transform 0.2s ease-in-out,
-    -webkit-transform 0.2s ease-in-out;
-  box-shadow:
-    0 0.125rem 9.375rem rgba(90, 97, 105, 0.1),
-    0 0.25rem 0.5rem rgba(90, 97, 105, 0.12),
-    0 0.9375rem 1.375rem rgba(90, 97, 105, 0.1),
-    0 0.4375rem 2.1875rem rgba(165, 182, 201, 0.1);
-}
-</style>
