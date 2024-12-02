4f34dc757537fc1577d4ee6671639bc38f55f33f
Windex <windeloira07@gmail.com>
2024-12-02 16:45:54 +0800

    feat(DashboardSidebarLogo): add img logo

--- src/components/dashboard/sidebar/DashboardSidebarLogo.vue
+++ src/components/dashboard/sidebar/DashboardSidebarLogo.vue
@@ -3,9 +3,13 @@
 <template>
   <RouterLink
     to="/dashboard"
-    class="navbar-brand w-100 mr-0 d-flex align-items-center justify-content-center overflow-hidden text-primary mb-12 fs-4 fw-bold"
-    ><i class="bi bi-shield-shaded me-3 fs-1"></i>Pest Control</RouterLink
-  >
+    class="navbar-brand w-100 mr-0 d-flex align-items-center justify-content-center overflow-hidden text-primary mb-10 fs-4 fw-bold"
+    ><img
+      src="../../../assets/images/logo.webp"
+      alt="Logo"
+      width="50px"
+      height="50px"
+  /></RouterLink>
 </template>
 
 <style scoped>
