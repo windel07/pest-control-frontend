86036bd1a5a9e6c6131672fbb70b0f52ca38b4a8
Windel <windeloira07@yahoo.com>
2024-12-21 21:29:37 +0800

    chore: change title to DripScan

--- src/components/dashboard/sidebar/DashboardSidebarLogo.vue
+++ src/components/dashboard/sidebar/DashboardSidebarLogo.vue
@@ -4,7 +4,7 @@
   <RouterLink
     to="/dashboard"
     class="navbar-brand w-100 mr-0 d-flex align-items-center justify-content-center overflow-hidden text-primary mb-12 fs-4 fw-bold"
-    ><i class="bi bi-shield-shaded me-3 fs-1"></i>Pest Control</RouterLink
+    ><i class="bi bi-shield-shaded me-3 fs-1"></i>DripScan</RouterLink
   >
 </template>
 
